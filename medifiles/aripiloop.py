bede74c6ce69b828cf16377fa1fc3923da3281d9
TLRKiliann <philogenie@protonmail.com>
2020-08-29 14:37:14 +0200

    abilify finished

--- medifiles/aripiloop.py
+++ medifiles/aripiloop.py
@@ -16,12 +16,27 @@ def ariFunc(self, drug3, drug4):
             if (drug3 == i or drug4 == i) and (drug3 == "carbamazépine" or \
                 drug4 == "carbamazépine" or drug3 == "tégrétol" or \
                 drug4 == "tégrétol"):
-                print("+ Interactions !!!")
+                print("+ Interactions carbamazépine !!!")
                 importAriCarba(self)
             elif (drug3 == i or drug4 == i) and (drug3 == "mae" or \
                 drug4 == "mae" or drug3 == "antiépileptiques" or \
                 drug4 == "antiépileptiques"):
-                print("+ Interactions !!!")
+                print("+ Interactions MAE !!!")
+                importAriCarba(self)
+            elif (drug3 == i or drug4 == i) and (drug3 == "vih" or \
+                drug3 == "névirapine" or drug3 == "éfavirenz" or \
+                drug4 == "vih" or drug4 == "névirapine" or drug4 == "éfavirenz"):
+                print("+ Interactions VIH !!!")
+                importAriCarba(self)
+            elif (drug3 == i or drug4 == i) and (drug3 == "atd" or \
+                drug3 == "antidépresseurs" or drug4 == "atd" or \
+                drug4 == "antidépresseurs"):
+                print("+ Interactions ATD !!!")
+                importAriAtd(self)
+            elif (drug3 == i or drug4 == i) and (drug3 == "antiTB" or \
+                drug3 == "antituberculeux" or drug4 == "antituberculeux" or \
+                drug4 == "antiTB"):
+                print("+ Interactions antiTB !!!")
                 importAriCarba(self)
             elif (drug3 == i or drug4 == i) and (drug3 == "primidone" or \
                 drug4 == "primidone" or drug3 == "mysoline" or \
@@ -31,64 +46,63 @@ def ariFunc(self, drug3, drug4):
             elif (drug3 == i or drug4 == i) and (drug3 == "phénobarbital" or \
                 drug4 == "phénobarbital" or drug3 == "aphénylbarbite" or \
                 drug4 == "aphénylbarbite"):
-                print("+ Interactions !!!")
+                print("+ Interactions phénobarbital !!!")
                 importAriCarba(self)
             elif (drug3 == i or drug4 == i) and (drug3 == "phénytoïne" or \
                 drug4 == "phénytoïne"):
-                print("+ Interactions !!!")
+                print("+ Interactions phénytoïne !!!")
                 importAriCarba(self)
             elif (drug3 == i or drug4 == i) and (drug3 == "depakine" or \
                 drug4 == "depakine" or drug3 == "valproate" or \
                 drug4 == "valproate"):
-                print("+ Interactions !!!")
+                print("+ Interactions depakine !!!")
                 importAriVal(self)
             elif (drug3 == i or drug4 == i) and (drug3 == "lithiofor" or \
                 drug4 == "lithiofor" or drug3 == "lithium" or \
                 drug4 == "lithium"):
-                print("+ Interactions !!!")
+                print("+ Interactions lithium !!!")
                 importAriVal(self)
             elif (drug3 == i or drug4 == i) and (drug3 == "isrs" or \
                 drug3 == "irsn" or drug4 == "isrs" or \
                 drug4 == "irsn"):
-                print("+ Interactions !!!")
-                importAriSrs(self)
+                print("+ Interactions irsn isrs !!!")
+                importAriSrsirsn(self)
             elif (drug3 == i or drug4 == i) and (drug3 == "fluoxétine" or \
-                drug4 == "fluoxétine"):
-                print("+ Interactions !!!")
-                importAriFluo(self)
-            elif (drug3 == i or drug4 == i) and (drug3 == "paroxétine" or \
-                drug4 == "paroxétine" or drug3 == "deroxat" or \
-                drug4 == "deroxat"):
-                print("+ Interactions !!!")
+                drug3 == "prozac" or drug4 == "fluoxétine" or drug4 == "prozac" or \
+                drug3 == "paroxétine" or drug3 == "deroxat" or \
+                drug4 == "paroxétine" or drug4 == "deroxat"):
+                print("+ Interactions paroxétine fluoxétine !!!")
                 importAriFluo(self)
             elif (drug3 == i or drug4 == i) and (drug3 == "apalu" or \
-                drug4 == "apalu" or drug3 == "quinidine" or \
+                drug3 == "quinidine" or drug4 == "apalu" or \
                 drug4 == "quinidine"):
-                print("+ Interactions !!!")
-                importAriFluo(self)
+                print("+ Interactions quinidine !!!")
+                importAriPalu(self)
             elif (drug3 == i or drug4 == i) and (drug3 == "kétoconazole" or \
                 drug3 == "keto" or drug4 == "kétoconazole" or drug4 == "keto"):
-                print("+ Interactions !!!")
+                print("+ Interactions kétoconazole !!!")
+                importAriKeto(self)
             elif (drug3 == i or drug4 == i) and (drug3 == "ahyperTA" or \
                 drug4 == "ahyperTA"):
-                print("+ Interactions !!!")
+                print("+ Interactions ahyperTA !!!")
                 importAriTa(self)
             elif (drug3 == i or drug4 == i) and (drug3 == "ac" or \
-                drug4 == "ac" or drug3 == "warfarine" or \
+                drug3 == "atuss" or drug3 == "warfarine" or \
                 drug4 == "warfarine" or \
                 drug3 == "bexine" or drug4 == "bexine" or \
                 drug3 == "dextrométhorphane" or \
-                drug4 == "dextrométhorphane"):
-                print("+ Interactions !!!")
+                drug4 == "dextrométhorphane" or drug4 == "ac" or \
+                drug4 == "atuss"):
+                print("+ Interactions ac atuss !!!")
                 importAriMiscel(self)
             elif (drug3 == i or drug4 == i) and (drug3 == "oméoprazole" or \
                 drug4 == "oméoprazole" or drug3 == "ipp" or \
                 drug4 == "ipp"):
-                print("+ Interactions !!!")
-                importAriFluo(self)
+                print("+ Interactions ipp !!!")
+                importAriMiscel(self)
             elif (drug3 == i or drug4 == i) and (drug3 == "oh" or \
                 drug3 == "alcool" or drug4 == "oh" or drug4 == "alcool"):
-                print("+ Interactions !!!")
+                print("+ Interactions oh !!!")
                 importAriOh(self)
             else:
                 print("+ End of aripiloop ttt list !")
--- medifiles/inter_pack/interact_abilify.py
+++ medifiles/inter_pack/interact_abilify.py
@@ -8,6 +8,20 @@ import subprocess
 
 
 # Display text in textbox from medifiles/interdrug (abilify + carba)
+def importAriAtd(self):
+    try:
+        if os.path.getsize('./medifiles/interdrug/ari_atd.txt'):
+            print("+ File 'ari_atd.txt' exist (read)!")
+            self.textBox.delete('0.0', 'end')
+            self.textBox.update()
+            with open('./medifiles/interdrug/ari_atd.txt', 'r') as textfile:
+                lines = textfile.readlines()
+                for li in lines:
+                    self.textBox.insert(END, li)
+    except FileNotFoundError as outnote:
+        print("+ Sorry, file 'ari_atd.txt' does not exist !", outnote)
+
+# Display text in textbox from medifiles/interdrug (abilify + carba)
 def importAriCarba(self):
     try:
         if os.path.getsize('./medifiles/interdrug/ari_carba.txt'):
@@ -20,34 +34,6 @@ def importAriCarba(self):
                     self.textBox.insert(END, li)
     except FileNotFoundError as outnote:
         print("+ Sorry, file 'ari_carba.txt' does not exist !", outnote)
-
-# Display text in textbox from medifiles/interdrug (abilify + valproate)
-def importAriVal(self):
-    try:
-        if os.path.getsize('./medifiles/interdrug/ari_valith.txt'):
-            print("+ File 'ari_valith.txt' exist (read)!")
-            self.textBox.delete('0.0', 'end')
-            self.textBox.update()
-            with open('./medifiles/interdrug/ari_valith.txt', 'r') as textfile:
-                lines = textfile.readlines()
-                for li in lines:
-                    self.textBox.insert(END, li)
-    except FileNotFoundError as outnote:
-        print("+ Sorry, file 'ari_valith.txt' does not exist !", outnote)
-
-# Display text in textbox from medifiles/interdrug (abilify + ISRS)
-def importAriSrs(self):
-    try:
-        if os.path.getsize('./medifiles/interdrug/ari_isrs.txt'):
-            print("+ File 'ari_isrs.txt' exist (read)!")
-            self.textBox.delete('0.0', 'end')
-            self.textBox.update()
-            with open('./medifiles/interdrug/ari_isrs.txt', 'r') as textfile:
-                lines = textfile.readlines()
-                for li in lines:
-                    self.textBox.insert(END, li)
-    except FileNotFoundError as outnote:
-        print("+ Sorry, file 'ari_isrs.txt' does not exist !", outnote)
 
 # Display text in textbox from medifiles/interdrug (abilify + fluoxétine)
 def importAriFluo(self):
@@ -62,20 +48,6 @@ def importAriFluo(self):
                     self.textBox.insert(END, li)
     except FileNotFoundError as outnote:
         print("+ Sorry, file 'ari_fluo.txt' does not exist !", outnote)
-
-# Display text in textbox from medifiles/interdrug (abilify + ahyperTA)
-def importAriTa(self):
-    try:
-        if os.path.getsize('./medifiles/interdrug/ari_ahyperta.txt'):
-            print("+ File 'ari_ahyperta.txt' exist (read)!")
-            self.textBox.delete('0.0', 'end')
-            self.textBox.update()
-            with open('./medifiles/interdrug/ari_ahyperta.txt', 'r') as textfile:
-                lines = textfile.readlines()
-                for li in lines:
-                    self.textBox.insert(END, li)
-    except FileNotFoundError as outnote:
-        print("+ Sorry, file 'ari_ahyperta.txt' does not exist !", outnote)
 
 # Display text in textbox from medifiles/interdrug (abilify + kétoconazole)
 def importAriKeto(self):
@@ -118,3 +90,59 @@ def importAriOh(self):
                     self.textBox.insert(END, li)
     except FileNotFoundError as outnote:
         print("+ Sorry, file 'ari_oh.txt' does not exist !", outnote)
+
+# Display text in textbox from medifiles/interdrug (abilify + paludéen)
+def importAriPalu(self):
+    try:
+        if os.path.getsize('./medifiles/interdrug/ari_palu.txt'):
+            print("+ File 'ari_palu.txt' exist (read)!")
+            self.textBox.delete('0.0', 'end')
+            self.textBox.update()
+            with open('./medifiles/interdrug/ari_palu.txt', 'r') as textfile:
+                lines = textfile.readlines()
+                for li in lines:
+                    self.textBox.insert(END, li)
+    except FileNotFoundError as outnote:
+        print("+ Sorry, file 'ari_palu.txt' does not exist !", outnote)
+
+# Display text in textbox from medifiles/interdrug (abilify + ISRS)
+def importAriSrsirsn(self):
+    try:
+        if os.path.getsize('./medifiles/interdrug/ari_isrs.txt'):
+            print("+ File 'ari_isrs.txt' exist (read)!")
+            self.textBox.delete('0.0', 'end')
+            self.textBox.update()
+            with open('./medifiles/interdrug/ari_isrs.txt', 'r') as textfile:
+                lines = textfile.readlines()
+                for li in lines:
+                    self.textBox.insert(END, li)
+    except FileNotFoundError as outnote:
+        print("+ Sorry, file 'ari_isrs.txt' does not exist !", outnote)
+
+# Display text in textbox from medifiles/interdrug (abilify + ahyperTA)
+def importAriTa(self):
+    try:
+        if os.path.getsize('./medifiles/interdrug/ari_ahyperta.txt'):
+            print("+ File 'ari_ahyperta.txt' exist (read)!")
+            self.textBox.delete('0.0', 'end')
+            self.textBox.update()
+            with open('./medifiles/interdrug/ari_ahyperta.txt', 'r') as textfile:
+                lines = textfile.readlines()
+                for li in lines:
+                    self.textBox.insert(END, li)
+    except FileNotFoundError as outnote:
+        print("+ Sorry, file 'ari_ahyperta.txt' does not exist !", outnote)
+
+# Display text in textbox from medifiles/interdrug (abilify + valproate)
+def importAriVal(self):
+    try:
+        if os.path.getsize('./medifiles/interdrug/ari_valith.txt'):
+            print("+ File 'ari_valith.txt' exist (read)!")
+            self.textBox.delete('0.0', 'end')
+            self.textBox.update()
+            with open('./medifiles/interdrug/ari_valith.txt', 'r') as textfile:
+                lines = textfile.readlines()
+                for li in lines:
+                    self.textBox.insert(END, li)
+    except FileNotFoundError as outnote:
+        print("+ Sorry, file 'ari_valith.txt' does not exist !", outnote)
