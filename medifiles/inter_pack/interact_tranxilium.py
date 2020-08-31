29b2706af3b7f53800221a832c5685947ee643fb
TLRKiliann <philogenie@protonmail.com>
2020-08-31 11:26:13 +0200

    tranxilium on work !

--- medifiles/inter_pack/interact_tranxilium.py
+++ medifiles/inter_pack/interact_tranxilium.py
@@ -7,16 +7,72 @@ import os
 import subprocess
 
 
-# Display text in textbox from medifiles/interdrug (leponex + carbamazépine)
-def importTranxi...(self):
+# Display text in textbox from medifiles/interdrug (tranxi + neuro)
+def importTranxiNeuro(self):
     try:
-        if os.path.getsize('./medifiles/interdrug/lepo_carba.txt'):
-            print("+ File 'lepo_carba.txt' exist (read)!")
+        if os.path.getsize('./medifiles/interdrug/tranxi_neuro.txt'):
+            print("+ File 'tranxi_neuro.txt' exist (read)!")
             self.textBox.delete('0.0', 'end')
             self.textBox.update()
-            with open('./medifiles/interdrug/lepo_carba.txt', 'r') as textfile:
+            with open('./medifiles/interdrug/tranxi_neuro.txt', 'r') as textfile:
                 lines = textfile.readlines()
                 for li in lines:
                     self.textBox.insert(END, li)
     except FileNotFoundError as outnote:
-        print("+ Sorry, file 'lepo_carba.txt' does not exist !", outnote)
+        print("+ Sorry, file 'tranxi_neuro.txt' does not exist !", outnote)
+
+# Display text in textbox from medifiles/interdrug (tranxi + bzd)
+def importTranxiBzd(self):
+    try:
+        if os.path.getsize('./medifiles/interdrug/tranxi_bzd.txt'):
+            print("+ File 'tranxi_bzd.txt' exist (read)!")
+            self.textBox.delete('0.0', 'end')
+            self.textBox.update()
+            with open('./medifiles/interdrug/tranxi_bzd.txt', 'r') as textfile:
+                lines = textfile.readlines()
+                for li in lines:
+                    self.textBox.insert(END, li)
+    except FileNotFoundError as outnote:
+        print("+ Sorry, file 'tranxi_bzd.txt' does not exist !", outnote)
+
+# Display text in textbox from medifiles/interdrug (tranxi + ipp)
+def importTranxiIpp(self):
+    try:
+        if os.path.getsize('./medifiles/interdrug/tranxi_ipp.txt'):
+            print("+ File 'tranxi_ipp.txt' exist (read)!")
+            self.textBox.delete('0.0', 'end')
+            self.textBox.update()
+            with open('./medifiles/interdrug/tranxi_ipp.txt', 'r') as textfile:
+                lines = textfile.readlines()
+                for li in lines:
+                    self.textBox.insert(END, li)
+    except FileNotFoundError as outnote:
+        print("+ Sorry, file 'tranxi_ipp.txt' does not exist !", outnote)
+
+# Display text in textbox from medifiles/interdrug (tranxi + opio)
+def importTranxiOpio(self):
+    try:
+        if os.path.getsize('./medifiles/interdrug/tranxi_opio.txt'):
+            print("+ File 'tranxi_opio.txt' exist (read)!")
+            self.textBox.delete('0.0', 'end')
+            self.textBox.update()
+            with open('./medifiles/interdrug/tranxi_opio.txt', 'r') as textfile:
+                lines = textfile.readlines()
+                for li in lines:
+                    self.textBox.insert(END, li)
+    except FileNotFoundError as outnote:
+        print("+ Sorry, file 'tranxi_opio.txt' does not exist !", outnote)
+
+# Display text in textbox from medifiles/interdrug (tranxi + oh)
+def importTranxiOh(self):
+    try:
+        if os.path.getsize('./medifiles/interdrug/tranxi_oh.txt'):
+            print("+ File 'tranxi_oh.txt' exist (read)!")
+            self.textBox.delete('0.0', 'end')
+            self.textBox.update()
+            with open('./medifiles/interdrug/tranxi_oh.txt', 'r') as textfile:
+                lines = textfile.readlines()
+                for li in lines:
+                    self.textBox.insert(END, li)
+    except FileNotFoundError as outnote:
+        print("+ Sorry, file 'tranxi_oh.txt' does not exist !", outnote)
--- medifiles/tranxiloop.py
+++ medifiles/tranxiloop.py
@@ -13,8 +13,61 @@ def tranxiFunc(self, drug3, drug4):
         drug1 VS drug2 (interactions with tranxilium)
         """
         if i == "tranxilium" or i == "clorazépate":
-            if (drug3 == i or drug4 == i) and (drug3 == "carbamazépine" or \
-                drug3 == "tégrétol" or drug4 == "carbamazépine" or \
-                drug4 == "tégrétol"):
-                print("+ Interactions carbamazépine !!!")
-                importTranxi...(self)
+            if (drug3 == i or drug4 == i) and (drug3 == "clozapine" or \
+                drug3 == "neuro" or drug3 == "apsy" or \
+                drug3 == "antipsychotiques" or drug4 == "apsy" or \
+                drug4 == "antipsychotiques" or drug4 == "clozapine" or \
+                drug4 == "neuro"):
+                print("+ Interactions clozapine et neuro !!!")
+                importTranxiNeuro(self)
+            elif (drug3 == i or drug4 == i) and (drug3 == "bzd" or \
+                drug3 == "benzodiazépines" or drug3 == "anxio" or \
+                drug3 == "anxiolytiques" or drug4 == "anxiolytiques" or \
+                drug4 == "anxio" or drug4 == "bzd" or \
+                drug4 == "benzodiazépines"):
+                print("+ Interactions bzd + anxio !!!")
+                importTranxiBzd(self)
+
+            elif (drug3 == i or drug4 == i) and (drug3 == "bzd" or \
+                drug3 == "benzodiazépines" or \
+                drug4 == "bzd" or drug4 == "benzodiazépines"):
+                print("+ Interactions bzd + anxio !!!")
+                importTranxiHypno(self)
+
+            elif (drug3 == i or drug4 == i) and (drug3 == "myo" or \
+                drug3 == "myorelaxants" or drug4 == "myorelaxants" or \
+                drug4 == "myo"):
+                print("+ Interactions bzd + anxio !!!")
+                importTranxiMyo(self)
+
+            elif (drug3 == i or drug4 == i) and (drug3 == "atd" or \
+                drug3 == "anxiolytiques" or drug3 == "anxio" or \
+                drug3 == "atuss" or drug3 == "antihist" or drug3 == "clonidine" or\
+                drug3 == "buprénorphine" or drug3 == "barbituriques" or \
+                drug3 == "antidépresseurs" or drug4 == "antidépresseurs" or \
+                drug4 == "buprénorphine" or drug4 == "barbituriques" or \
+                drug4 == "atuss" or drug4 == "antihist" or \
+                drug4 == "anxio" or drug4 == "atd" or drug4 == "clonidine" or \
+                drug4 == "anxiolytiques"):
+                print("+ Interactions bzd + anxio !!!")
+                importTranxiGroup(self)
+
+            elif (drug3 == i or drug4 == i) and (drug3 == "ipp" or \
+                drug3 == "cimétidine" or drug3 == "oméoprazole" or \
+                drug4 == "oméoprazole" or drug4 == "ipp" or \
+                drug4 == "cimétidine"):
+                print("+ Interactions ipp !!!")
+                importTranxiIpp(self)
+
+            elif (drug3 == i or drug4 == i) and (drug3 == "opio" or \
+                drug3 == "opioïdes" or drug3 == "meoh" or \
+                drug3 == "méthadone" or drug4 == "meoh" or \
+                drug4 == "opio" or drug4 == "méthadone" or \
+                drug4 == "opioïdes"):
+                print("+ Interactions opio !!!")
+                importTranxiOpio(self)
+            elif (drug3 == i or drug4 == i) and (drug3 == "oh" or \
+                drug3 == "alcool" or drug4 == "alcool" or \
+                drug4 == "oh"):
+                print("+ Interactions oh !!!")
+                importTranxiOh(self)
