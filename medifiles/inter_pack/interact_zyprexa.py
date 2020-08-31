85d5eb3753bf5e6f9811cc3fd9fcc6c47e74f5ed
TLRKiliann <philogenie@protonmail.com>
2020-08-31 13:23:59 +0200

    zyprexa on work !

--- medifiles/inter_pack/interact_zyprexa.py
+++ medifiles/inter_pack/interact_zyprexa.py
@@ -7,16 +7,58 @@ import os
 import subprocess
 
 
-# Display text in textbox from medifiles/interdrug (abilify + carba)
-def importAriAtd(self):
+# Display text in textbox from medifiles/interdrug (zyprexa + neuro)
+def importZyprexMae(self):
     try:
-        if os.path.getsize('./medifiles/interdrug/ari_atd.txt'):
-            print("+ File 'ari_atd.txt' exist (read)!")
+        if os.path.getsize('./medifiles/interdrug/zyprexa_neuro.txt'):
+            print("+ File 'zyprexa_neuro.txt' exist (read)!")
             self.textBox.delete('0.0', 'end')
             self.textBox.update()
-            with open('./medifiles/interdrug/ari_atd.txt', 'r') as textfile:
+            with open('./medifiles/interdrug/zyprexa_neuro.txt', 'r') as textfile:
                 lines = textfile.readlines()
                 for li in lines:
                     self.textBox.insert(END, li)
     except FileNotFoundError as outnote:
-        print("+ Sorry, file 'ari_atd.txt' does not exist !", outnote)
+        print("+ Sorry, file 'zyprexa_neuro.txt' does not exist !", outnote)
+
+# Display text in textbox from medifiles/interdrug (zyprexa + atd)
+def importZyprexAtd(self):
+    try:
+        if os.path.getsize('./medifiles/interdrug/zyprexa_atd.txt'):
+            print("+ File 'zyprexa_atd.txt' exist (read)!")
+            self.textBox.delete('0.0', 'end')
+            self.textBox.update()
+            with open('./medifiles/interdrug/zyprexa_atd.txt', 'r') as textfile:
+                lines = textfile.readlines()
+                for li in lines:
+                    self.textBox.insert(END, li)
+    except FileNotFoundError as outnote:
+        print("+ Sorry, file 'zyprexa_atd.txt' does not exist !", outnote)
+
+# Display text in textbox from medifiles/interdrug (zyprexa + tabac)
+def importZyprexTabac(self):
+    try:
+        if os.path.getsize('./medifiles/interdrug/zyprexa_tabac.txt'):
+            print("+ File 'zyprexa_tabac.txt' exist (read)!")
+            self.textBox.delete('0.0', 'end')
+            self.textBox.update()
+            with open('./medifiles/interdrug/zyprexa_tabac.txt', 'r') as textfile:
+                lines = textfile.readlines()
+                for li in lines:
+                    self.textBox.insert(END, li)
+    except FileNotFoundError as outnote:
+        print("+ Sorry, file 'zyprexa_tabac.txt' does not exist !", outnote)
+
+# Display text in textbox from medifiles/interdrug (zyprexa + oh)
+def importZyprexOh(self):
+    try:
+        if os.path.getsize('./medifiles/interdrug/zyprexa_oh.txt'):
+            print("+ File 'zyprexa_oh.txt' exist (read)!")
+            self.textBox.delete('0.0', 'end')
+            self.textBox.update()
+            with open('./medifiles/interdrug/zyprexa_oh.txt', 'r') as textfile:
+                lines = textfile.readlines()
+                for li in lines:
+                    self.textBox.insert(END, li)
+    except FileNotFoundError as outnote:
+        print("+ Sorry, file 'zyprexa_oh.txt' does not exist !", outnote)
--- medifiles/zyprexaloop.py
+++ medifiles/zyprexaloop.py
@@ -13,11 +13,31 @@ def zyprexFunc(self, drug3, drug4):
         drug1 VS drug2 (interactions with abilify)
         """
         if i == "abilify" or i == "aripiprazol":
-            if (drug3 == i or drug4 == i) and (drug3 == "" or \
-                drug4 == "" or drug3 == "" or \
-                drug4 == ""):
-                print("+ Interactions ... !!!")
-                importZyprexNeuro(self)
-
+            if (drug3 == i or drug4 == i) and (drug3 == "phénytoïne" or \
+                drug3 == "phénobarbital" or drug3 == "aphénylbarbite" or \
+                drug3 == "anti-épileptiques" or drug3 == "mae" or \
+                drug4 == "phénytoïne" or drug4 == "mae" or \
+                drug4 == "aphénylbarbite" or drug4 == "anti-épileptiques" or \
+                drug4 == "phénobarbital"):
+                print("+ Interactions mae !!!")
+                importZyprexMae(self)
+            elif (drug3 == i or drug4 == i) and (drug3 == "atd" or \
+                drug3 == "fluvoxamine" or drug3 == "antidépresseurs" or \
+                drug3 == "prozac" or drug3 == "floxyfral" or
+                drug3 == "fluanxol" or drug4 == "atd" or \
+                drug4 == "fluvoxamine" or drug4 == "antidépresseurs" or \
+                drug4 == "prozac" or drug4 == "fluanxol" or \
+                drug4 == "floxyfral"):
+                print("+ Interactions atd !!!")
+                importZyprexAtd(self)
+            elif (drug3 == i or drug4 == i) and (drug3 == "tabac" or \
+                drug4 == "tabac"):
+                print("+ Interactions tabac !!!")
+                importZyprexTabac(self)
+            elif (drug3 == i or drug4 == i) and (drug3 == "oh" or \
+                drug3 == "alcool" or drug4 == "oh" or \
+                drug4 == "alcool"):
+                print("+ Interactions oh !!!")
+                importZyprexOh(self)
             else:
                 print("+ End of zyprexaloop ttt list !")
